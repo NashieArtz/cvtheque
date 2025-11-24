13903ce63ccd259e6d40f9773c8267cc54591cd1
Mathieu Leboucher <mathieu.leboucher76@gmail.com>
2025-11-24 12:11:56 +0100

    add header and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,11 @@
+<link href="../assets/css/header-footer.css" rel="stylesheet" >
+
+<footer>
+    <nav>
+        <a href='#'>About</a>
+        <a href='#'>Contact</a>
+        <a href='#'>Privacy Policy</a>
+    </nav>
+</footer>
+
 <?php

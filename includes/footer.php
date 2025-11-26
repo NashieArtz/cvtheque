65e40ccf48ab6a0c11fa287f2c911aa1bbaf70cc
WU Ange <wu.ange@nfs.school>
2025-11-26 13:31:43 +0100

    Ajout des pages et logiques des pages

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+
+
+<footer>
+    <nav>
+        <a href='#'>About</a>
+        <a href='#'>Contact</a>
+        <a href='#'>Privacy Policy</a>
+    </nav>
+</footer>
+
+<script src="./assets/js/index.js"></script>
+<?php

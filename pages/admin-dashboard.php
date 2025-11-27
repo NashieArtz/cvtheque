b729f1885b0f00d6ee434586f927abb8cd46d4ee
Florent ZYSK <zysk.florent@nfs.school>
2025-11-27 16:00:24 +0100

    acces to admin page restricted to admin role_id

--- pages/admin-dashboard.php
+++ pages/admin-dashboard.php
@@ -1,1 +1,7 @@
+Porta maximus elit diam, at, nulla velit suscipit efficitur id finibus ac. Ultricies faucibus purus dolor pellentesque
+pellentesque curabitur tortor, nibh, massa pretium etiam. Purus velit suspendisse sed, rutrum consequat dui etiam
+ullamcorper lorem sit efficitur. Justo dignissim pretium praesent lacus, sagittis, auctor fermentum lectus condimentum
+urna ultricies. In ac.
 <?php
+
+include './config/roles/admin.php';

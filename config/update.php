e2ee74d8759b4d1adbe25cf53c16c836abf81c6b
Florent ZYSK <zysk.florent@nfs.school>
2025-12-05 09:35:37 +0100

    fixed user skill update function

--- config/update.php
+++ config/update.php
@@ -1,46 +1,43 @@
 <?php
 
 if (isset($_SESSION['user']) && is_array($_SESSION['user'])) {
-    function update(PDO $pdo, $table, $column, $new_value)
-    {
-        if (isset($_POST) && !empty($_POST)) {
-            $col = ' ';
-            $value = ' ';
-            $user_id = ($_SESSION['user']['id']);
-            foreach ($column as $col) {
-                $col += "`$col`, ";
-                $value += '?, ';
-            }
-            $sql = "UPDATE `$table`(`$col`) SET VALUE ($value) WHERE `id` = $user_id";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute($new_value);
-        }
+  function update(PDO $pdo, $table, $column, $new_value)
+  {
+    if (isset($_POST) && !empty($_POST)) {
+      $col = ' ';
+      $value = ' ';
+      $user_id = ($_SESSION['user']['id']);
+      foreach ($column as $col) {
+        $col += "`$col`, ";
+        $value += '?, ';
+      }
+      $sql = "UPDATE `$table`(`$col`) SET VALUE ($value) WHERE `id` = $user_id";
+      $stmt = $pdo->prepare($sql);
+      $stmt->execute($new_value);
     }
+  };
+  function updateSkills(PDO $pdo, $skillType)
+  {
+    if (isset($_POST) && !empty($_POST)) {
 
-    ;
-    function updateSkills(PDO $pdo, $skillType)
-    {
-        if (isset($_POST) && !empty($_POST)) {
+      $user_id = ($_SESSION['user']['id']);
+      $skillInput = htmlspecialchars(trim($_POST[$skillType]));
+      $sqlSelect = "SELECT id FROM `skills` WHERE '$skillType'.name LIKE '$skillInput'";
+      $skill_id = $pdo->query($sqlSelect)->fetchAll();
 
-            $user_id = ($_SESSION['user']['id']);
-            $skillInput = htmlspecialchars(trim($_POST[$skillType]));
-            $sqlSelect = "SELECT * FROM `skills` WHERE '$skillType'.name LIKE '$skillInput'";
-            $skill = $pdo->query($sqlSelect)->fetchAll();
-
-            if ($skill) {
-                $sqlUpdateUser = "UPDATE `user`(`skill_id`) WHERE id = '$user_id'";
-                $stmt = $pdo->prepare($sqlUpdateUser);
-                $stmt->execute($skill['id']);
-            } else {
-                $sqlUpdateSkill = "INSERT INTO `skills`(`$skillType`) VALUES (?)";
-                $stmtSkill = $pdo->prepare($sqlUpdateSkill);
-                $stmtSkill->execute([$skillInput]);
-                $skill_id = $pdo->lastInsertId();
-                $sqlUpdateUser = "UPDATE `user`(`skill_id`) SET VALUE ? WHERE id = '$user_id'";
-                $stmtUser = $pdo->prepare($sqlUpdateUser);
-                $stmtUser->execute([$skill_id]);
-            }
-        }
-
+      if ($skill_id) {
+        $sqlUserSkill = "INSERT INTO `user_has_skills`(`user_has_skills_id`,`skills_id`) VALUE (?,?)";
+        $stmt = $pdo->prepare($sqlUserSkill);
+      } else {
+        $sqlUpdateSkill = "INSERT INTO `skills`(`$skillType`) VALUE ?";
+        $stmtSkill = $pdo->prepare($sqlUpdateSkill);
+        $stmtSkill->execute([$skillInput]);
+        $skill_id = $pdo->lastInsertId();
+        $sqlUserSkill = "INSERT INTO `user_has_skills`(`user_has_skills_id`,`skills_id`) VALUE (?,?)";
+        $stmt = $pdo->prepare($sqlUserSkill);
+        $stmt->execute([$user_id][$skill_id]);
+      }
     }
-}
+  }
+};
+
